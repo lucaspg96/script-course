c88697920344123edae1bbaa9d9159e2dc2c3338
Lucas Peres <peres.ufc@gmail.com>
2017-09-26 14:57:22 -0300

    Starting javascript-work1

---
+++ javascript/work1/qst1.js
@@ -0,0 +1,11 @@
+class Sorter{
+	constructor(){}
+
+	sort(list,reverse=false){
+		return reverse ? list.sort().reverse() : list.sort();
+	}
+}
+
+var sorter = new Sorter();
+
+console.log(sorter.sort([1,5,0,-3]))
